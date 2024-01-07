d582582e0d57a690d64de184beb6073655ac7ac0
interstellarninja <ninjainterstellar@gmail.com>
2024-01-07 01:45:02 -0500

    adding shell script to run datagen pipeline

---
+++ run_datagen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# source conda init script
+source /home/interstellarninja/anaconda3/etc/profile.d/conda.sh
+conda activate data-gen
+
+# initialize redis vectordb on docker
+./run_vectordb.sh
+
+# run data correction pipeline
+python scripts/datagen.py --ai_vendor openai --num_results 10 --num_tasks 10
