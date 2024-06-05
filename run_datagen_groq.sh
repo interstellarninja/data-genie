e0fb779f670138cb5253eec34cda4c0b000dee1f
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-06-05 14:08:03 +0200

    feat: Removed device specific conda init

--- run_datagen_groq.sh
+++ run_datagen_groq.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# source conda init script
-source /home/interstellarninja/anaconda3/etc/profile.d/conda.sh
-conda activate data-gen
 
 # initialize redis vectordb on docker
 ./run_vectordb.sh
